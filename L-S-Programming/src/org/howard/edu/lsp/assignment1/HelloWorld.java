2714ff61f190d26dabd74363583f9e2d89e5cc8e
suyogbala <suyogbala@suyogs-mbp>
2024-01-20 14:20:19 -0500

    Assignment #1

---
+++ L-S-Programming/src/org/howard/edu/lsp/assignment1/HelloWorld.java
@@ -0,0 +1,14 @@
+/**
+ * Name: Suyog Bala
+ */
+
+package org.howard.edu.lsp.assignment1;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		System.out.println("Hello World!!");
+
+	}
+
+}
