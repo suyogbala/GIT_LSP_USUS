22e0039720e46bab375c25a57413363380d5ca5b
suyogbala <suyogbala@suyogs-mbp.fios-router.home>
2024-03-14 21:06:40 -0400

    qn 2

--- L-S-Programming/src/org/howard/edu/lsp/midterm/question2/RangeTester.java
+++ L-S-Programming/src/org/howard/edu/lsp/midterm/question2/RangeTester.java
@@ -5,18 +5,14 @@ public class RangeTester {
         // Test IntegerRange
         IntegerRange range1 = new IntegerRange(2, 19);
         IntegerRange range2 = new IntegerRange(3, 21);
-        IntegerRange range3 = new IntegerRange(14, 25);
-
+        
         System.out.println("Range 1 size: " + range1.size());
         System.out.println("Range 2 size: " + range2.size());
-        System.out.println("Range 3 size: " + range3.size());
 
         System.out.println("Range 1 contains 1: " + range1.contains(1));
         System.out.println("Range 2 contains 4: " + range2.contains(4));
-        System.out.println("Range 3 contains 14: " + range3.contains(14));
 
         System.out.println("Range 1 overlaps with Range 2: " + range1.overlaps(range2));
-        System.out.println("Range 1 overlaps with Range 3: " + range1.overlaps(range3));
 
         // Test equals method
         IntegerRange range4 = new IntegerRange(2, 19);
