133ae385bd473b95e3c0596f0a622246ab1a3bf7
suyogbala <suyogbala@suyogs-mbp.fios-router.home>
2024-03-14 21:12:08 -0400

    final

--- L-S-Programming/src/org/howard/edu/lsp/midterm/question2/Range.java
+++ L-S-Programming/src/org/howard/edu/lsp/midterm/question2/Range.java
@@ -1,40 +1,14 @@
 package org.howard.edu.lsp.midterm.question2;
 
 public interface Range {
-    /**
-     * Returns true if value is between the lower and upper bounds (inclusive).
-     *
-     * @param value the value to check
-     * @return true if value is within the range, false otherwise
-     */
+	
     boolean contains(int value);
-
-    /**
-     * Returns true if the receiver contains at least one value in common with another range.
-     *
-     * @param other the other range to check for overlap
-     * @return true if the ranges overlap, false otherwise
-     */
-    boolean overlaps(Range other);
-
-    /**
-     * Returns the number of integers in the range.
-     *
-     * @return the size of the range
-     */
-    int size();
     
-    /**
-     * Gets the lower bound of the range.
-     *
-     * @return the lower bound
-     */
+    boolean overlaps(Range other);
+    
+    int size();
+
     int getLowerBound();
     
-    /**
-     * Gets the upper bound of the range.
-     *
-     * @return the upper bound
-     */
     int getUpperBound();
 }
--- L-S-Programming/src/org/howard/edu/lsp/midterm/question2/RangeTester.java
+++ L-S-Programming/src/org/howard/edu/lsp/midterm/question2/RangeTester.java
@@ -2,7 +2,7 @@ package org.howard.edu.lsp.midterm.question2;
 
 public class RangeTester {
     public static void main(String[] args) {
-        // Test IntegerRange
+    	
         IntegerRange range1 = new IntegerRange(2, 19);
         IntegerRange range2 = new IntegerRange(3, 21);
         
@@ -13,8 +13,7 @@ public class RangeTester {
         System.out.println("Range 2 contains 4: " + range2.contains(4));
 
         System.out.println("Range 1 overlaps with Range 2: " + range1.overlaps(range2));
-
-        // Test equals method
+        
         IntegerRange range4 = new IntegerRange(2, 19);
         System.out.println("Range 1 equals Range 4: " + range1.equals(range4));
     }
